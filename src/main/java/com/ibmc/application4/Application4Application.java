349d0e2344c9518c6ef940626062d7a0ea64fd04
BinduSK <bindusajithkumar01@gmail.com>
2022-07-24 18:10:08 +0530

    first commit

---
+++ src/main/java/com/ibmc/application4/Application4Application.java
@@ -0,0 +1,13 @@
+package com.ibmc.application4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibmc/application4/Application4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibmc.application4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
